c82c44c12f4123ed4ce6735079681e9b84705f0b
jegger <jegger@hsr.ch>
2020-03-25 11:06:30 +0100

    first commit

--- webapp-create.bat
+++ webapp-create.bat
@@ -5,5 +5,5 @@ cmd /c az group create --location westus --name lakeside-rg
 pause
 cmd /c az appservice plan create -g lakeside-rg -n lakeside-service-plan --is-linux
 pause
-cmd /c az webapp create -g lakeside-rg -p lakeside-service-plan -n %1 --runtime "JAVA|8-jre8"
+cmd /c az webapp create -g lakeside-rg -p lakeside-service-plan -n %1 --runtime "node|6.2"
 pause
