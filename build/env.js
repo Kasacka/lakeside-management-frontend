d3253406c1d23a64ae63b6f4a3a60eb03df1a3c4
jegger <jegger@hsr.ch>
2020-03-25 17:46:48 +0100

    deploy env.js as a workaround

---
+++ build/env.js
@@ -0,0 +1,1 @@
+window._env = {"NODE_ENV":"development","REACT_APP_CUSTOMER_MANAGEMENT_BACKEND":"http://lakeside-management-backend.azurewebsites.net/","REACT_APP_CUSTOMER_SELF_SERVICE_BACKEND":"http://localhost:8080"};
